a2a32f795f1b75c214c7e6d5b4dfc8170d618f08
mitchelltaylor159 <mitchelltaylor159@gmail.com>
2018-03-01 21:09:49 -0700

    Vocab Prototype 1
    Layout established.
    Audio plays on click.
    Hotpoint scales to word size.
    Screen scales to size and to rotation.
    UI overlay scales to screen.

---
+++ Assets/Scripts/VocabController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VocabController : MonoBehaviour {
+
+	public string[] englishWords;
+	public string[] thaiWords;
+	public AudioClip[] englishAudioClips;
+	public AudioClip[] thaiAudioClips;
+
+	private int currentIndex;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void proceed (int i)
+	{
+		currentIndex += i;
+	}
+}
---
+++ Assets/Scripts/WordController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordController : MonoBehaviour {
+
+	public AudioClip audio;
+	public string word;
+
+	private GameObject audioPlayer;
+	private TextMesh textMesh;
+	private BoxCollider2D box;
+
+	// Use this for initialization
+	void Start () {
+		audioPlayer = GameObject.Find("Audio Player");
+		textMesh = GetComponent<TextMesh>();
+		box = GetComponent<BoxCollider2D>();
+		UpdateWord(word);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void UpdateWord(string word)
+	{
+		this.word = word;
+		textMesh.text = word;
+
+		// Update the hotpoint size to match the word size.
+		var bounds = textMesh.GetComponent<Renderer>().bounds;
+		box.size = bounds.size / transform.localScale.x;
+	}
+
+	void OnMouseUp()
+	{
+		Debug.Log("Audio");
+		audioPlayer.GetComponent<AudioSource>().PlayOneShot(audio);
+	}
+}
