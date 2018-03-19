bf36e674b10b65f99b95bcd6279cacf6c45d9e84
Jan Taylor <janttaylor@gmail.com>
2018-03-19 08:32:11 -0600

    Stop audio from auto playing

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryController : MonoBehaviour {
 
@@ -30,10 +31,65 @@ public class StoryController : MonoBehaviour {
     public AudioClip sentence;
 
     /// <summary>
+    /// Boolean for if the menu is hidden
+    /// </summary>
+    public bool menuHidden;
+
+    /// <summary>
+    /// Game object for the back button
+    /// </summary>
+    public Image backButton;
+
+    /// <summary>
+    /// Game Object for the forward button
+    /// </summary>
+    public Image forwardButton;
+
+    /// <summary>
+    /// Game Object for the menu button
+    /// </summary>
+    public Image menuButton;
+
+    /// <summary>
+    /// Game object for the normal sound sentance clip
+    /// </summary>
+    public Image normalSound;
+
+    /// <summary>
+    /// Game object for the slowed down sentance clip
+    /// </summary>
+    public Image slowSound;
+
+    /// <summary>
     /// Play sentence when loading the scene
     /// </summary>
     public void Start() {
-        PlaySentence();
+        HideMenu();
+        //PlaySentence(); // Removed on request on sprint 1
+    }
+
+    /// <summary>
+    /// Show the menu but the back button
+    /// </summary>
+    public void ShowMenu() {
+        int activeScene = GameState.Instance.ActiveScene;
+        // don't show the back button on first scenes of story
+        if (activeScene != 7 && activeScene != 15 && activeScene != 23 && activeScene != 31) {
+            backButton.enabled = true;
+        }
+        forwardButton.enabled = true;
+        //normalSound.enabled = true;
+        //slowSound.enabled = true;
+    }
+
+    /// <summary>
+    /// Hide the menu but the back button
+    /// </summary>
+    public void HideMenu() {
+        backButton.enabled = true;
+        forwardButton.enabled = true;
+        //normalSound.enabled = true;
+        //slowSound.enabled = true;
     }
 
     /// <summary>
@@ -41,7 +97,8 @@ public class StoryController : MonoBehaviour {
     /// </summary>
     public void PlayWordOne() {
         if (audioSource.isPlaying)
-            audioSource.Stop();
+            // audioSource.Stop(); - Sprint 1 fix
+            return;
         audioSource.PlayOneShot(word);
     }
 
@@ -50,7 +107,8 @@ public class StoryController : MonoBehaviour {
     /// </summary>
     public void PlayWordTwo() {
         if (audioSource.isPlaying)
-            audioSource.Stop();
+            // audioSource.Stop(); - Sprint 1 fix
+            return;
         audioSource.PlayOneShot(word2);
     }
 
@@ -59,7 +117,8 @@ public class StoryController : MonoBehaviour {
     /// </summary>
     public void PlayWordThree() {
         if (audioSource.isPlaying)
-            audioSource.Stop();
+            // audioSource.Stop(); - Sprint 1 fix
+            return;
         audioSource.PlayOneShot(word3);
     }
 
@@ -68,7 +127,11 @@ public class StoryController : MonoBehaviour {
     /// </summary>
     public void PlaySentence() {
         if (audioSource.isPlaying)
-            audioSource.Stop();
+            //audioSource.Stop(); - Sprint 1 fix
+            return;
         audioSource.PlayOneShot(sentence);
+
+        // Once the sentance has been played, show the menu to control scene
+        ShowMenu();
     }
 }
