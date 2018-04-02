d8903041fa0da8858234efcddf824a944f96f5c8
Jan Taylor <janttaylor@gmail.com>
2018-04-01 23:15:21 -0600

    Background, water, and click to shoot

---
+++ Assets/Scripts/BonusGameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusGameController : MonoBehaviour {
+
+    /// <summary>
+    /// Water particle system
+    /// </summary>
+    public ParticleSystem water;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+
+    /// <summary>
+    /// Restarts/starts particle system
+    /// </summary>
+    public void Fire() {
+        water.Stop();
+        water.Play();
+    }
+}
