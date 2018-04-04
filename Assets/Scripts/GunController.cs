a9ca9f96fba919f68f1d1baf3b40dd5d71b1aa6b
Jan Taylor <janttaylor@gmail.com>
2018-04-03 21:28:53 -0600

    Gangster shooting

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float turnRateRadians = 2 * Mathf.PI;
+
+    void Update() {
+        // Get mouse click - Input.mousePosition
+
+        //Vector3 targetDirection = Input.mousePosition - transform.position;
+        //targetDirection.z = 0f;
+        //targetDirection = targetDirection.normalized;
+
+        //Vector3 currentDirection = transform.forward;
+        //currentDirection = Vector3.RotateTowards(currentDirection, targetDirection, turnRateRadians * Time.deltaTime, .5f);
+
+        //Quaternion qDirection = new Quaternion();
+        //qDirection.SetLookRotation(currentDirection, transform.forward);
+        ////qDirection = new Quaternion(qDirection.x, qDirection.y, 0f, 0f);
+        //transform.rotation = qDirection;
+
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10.0f; //The distance from the camera to the player object
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 }
