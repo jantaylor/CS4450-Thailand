18672876413d6e780377c48d07a138b45ff40fae
Jan Taylor <janttaylor@gmail.com>
2018-04-03 21:39:24 -0600

    Mouse rotation

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,25 +9,16 @@ public class GunController : MonoBehaviour {
     private float turnRateRadians = 2 * Mathf.PI;
 
     void Update() {
-        // Get mouse click - Input.mousePosition
+        // Get mouse position and set Z to far back
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 20.0f;
 
-        //Vector3 targetDirection = Input.mousePosition - transform.position;
-        //targetDirection.z = 0f;
-        //targetDirection = targetDirection.normalized;
-
-        //Vector3 currentDirection = transform.forward;
-        //currentDirection = Vector3.RotateTowards(currentDirection, targetDirection, turnRateRadians * Time.deltaTime, .5f);
-
-        //Quaternion qDirection = new Quaternion();
-        //qDirection.SetLookRotation(currentDirection, transform.forward);
-        ////qDirection = new Quaternion(qDirection.x, qDirection.y, 0f, 0f);
-        //transform.rotation = qDirection;
-
-        var mousePos = Input.mousePosition;
-        mousePos.z = 10.0f; //The distance from the camera to the player object
+        // Set look position to camera's point and calculate the transform difference
         Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
         lookPos = lookPos - transform.position;
-        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Rotate the axis
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = rotation;
     }
 }
