7e99888f34279cf86323ee4c231c941297e996c9
Jan Taylor <janttaylor@gmail.com>
2018-04-14 10:01:15 -0600

    working on progress bar

---
+++ Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour {
+
+    public float CurrentScene { get; set; }
+
+    public float MaxScenes { get; set; }
+
+    public Slider progressBar;
+
+	// Use this for initialization
+	void Start () {
+        // Not all scenes have progress bars
+
+        // Stories are 8 scenes long
+        MaxScenes = 8f;
+
+        progressBar.value = CalculateProgress();
+	}
+
+    public float CalculateProgress() {
+        return CurrentScene / MaxScenes;
+    }
+}
